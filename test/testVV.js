df104bee2c6a73eb782ae427f980c69ca72c38e6
Chat-Wane <brice.nedelec@gmail.com>
2014-03-04 18:11:12 +0100

    version vector without init size

--- test/testVV.js
+++ test/testVV.js
@@ -6,23 +6,9 @@ var VV = require('../lib/versionvector.js');
 
 describe('versionvector.js', function() {
 
-    describe('vv', function(){
-	it('the size', function(){
-	    var vv = new VV(13,37);
-	    expect(vv._v).to.have.length(37);
-	});
-	
-	it('init the entries to zero', function(){
-	    var vv = new VV(13,37);
-	    for (var i=0; i<vv._v.length; ++i){
-		expect(vv._v[i]).to.be.eql(0);
-	    };
-	});
-    });
-
     describe('increment', function(){
 	it('increment the entry', function(){
-	    var vv = new VV(13,37);
+	    var vv = new VV(13);
 	    vv.increment();
 	    expect(vv._v[vv._e]).to.be.eql(1);
 	});
@@ -30,10 +16,9 @@ describe('versionvector.js', function() {
 
     describe('incrementfrom', function(){
 	it('increment the entry from another vv', function(){
-	    var vv = new VV(13,37);
-	    var remoteVV = new VV(4,37);
+	    var vv = new VV(13);
+	    var remoteVV = new VV(4);
 	    remoteVV.increment();
-	    expect(vv._v[remoteVV._e]).to.be.eql(0);
 	    vv.incrementFrom(remoteVV);
 	    expect(remoteVV._v[remoteVV._e]).to.be.eql(1);
 	    expect(vv._v[remoteVV._e]).to.be.eql(remoteVV._v[remoteVV._e]);
@@ -42,8 +27,8 @@ describe('versionvector.js', function() {
     
     describe('isReady', function(){
 	it('check if the vv is ready', function(){
-	    var vv = new VV(13,37);
-	    var vv2 = new VV(0,37);
+	    var vv = new VV(13);
+	    var vv2 = new VV(0);
 	    vv2.increment();
 	    expect(vv.isRdy(vv2)).to.be.ok();
 	    vv2.increment();
@@ -53,8 +38,8 @@ describe('versionvector.js', function() {
 
     describe('isLower', function(){
 	it('check if the vv has been seen before or not', function(){
-	    var vv = new VV(13,37);
-	    var vv2 = new VV(0,37);
+	    var vv = new VV(13);
+	    var vv2 = new VV(0);
 	    vv2.increment();
 	    expect(vv.isLower(vv2)).to.not.be.ok();
 	    vv.incrementFrom(vv2);
